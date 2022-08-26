f55374cd34356daa5c92b9b6ddf5901d017ea94b
Lucas <tinwen.dev@gmail.com>
2022-08-26 12:39:00 +0000

    missing egld tx after all esdt/nft has been sent

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Tuple
 import requests
 from erdpy.accounts import Address
 from erdpy.transactions import Transaction
@@ -13,6 +14,15 @@ def get_esdt(address:Address,network:str) -> str :
     
     return requests.get(url).json()["data"]["esdts"]
 
+def get_egld_balance(address:Address,network:str)->str:
+  if network=="1":
+      url_egld = "https://api.elrond.com/accounts/"+address.bech32()
+  elif network=="D":
+    url_egld = "https://devnet-api.elrond.com/accounts/"+address.bech32()
+  elif network=="T":
+    url_egld = "https://testnet-api.elrond.com/accounts/"+address.bech32()
+  
+  return requests.get(url_egld).json()["balance"]
 def set_esdt_nft_transfer(transaction:Transaction,receiver:Address,sender:Address,collection:str, id_nft:str,quantity:str):
   #Ticker de la collection, puis id du nft, puis qty et enfin l'adresse du receiver
   transaction.receiver = sender.bech32()
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import helper
 
 def main():
     #python3 main.py --receiver=erd1yqv7q0khhrlxc8w0q8cv85ddlur8wdsu5q39kazpkhmyrdmnrzyqkkkqzg --sender=wallet2.pem
+    #python3 main.py --receiver=erd1u64t98v0n4d8ayx6r9nmwrpzvxezyzyz5ewn80ulhhuj02x9ragqp6ffa2 --sender=wallet.pem
     logging.basicConfig(level=logging.ERROR)
     parser = ArgumentParser()
     parser.add_argument("--receiver", help="receiver address", required=True)
@@ -28,9 +29,9 @@ def main():
 
     # proxy = ElrondProxy("https://devnet-gateway.elrond.com")
     # network = proxy.get_network_config()
-    # receiver = Address("erd1yqv7q0khhrlxc8w0q8cv85ddlur8wdsu5q39kazpkhmyrdmnrzyqkkkqzg")
-    # # receiver = Address("erd1u64t98v0n4d8ayx6r9nmwrpzvxezyzyz5ewn80ulhhuj02x9ragqp6ffa2")
-    # sender = Account(pem_file="wallet2.pem")
+    # # receiver = Address("erd1yqv7q0khhrlxc8w0q8cv85ddlur8wdsu5q39kazpkhmyrdmnrzyqkkkqzg")
+    # receiver = Address("erd1u64t98v0n4d8ayx6r9nmwrpzvxezyzyz5ewn80ulhhuj02x9ragqp6ffa2")
+    # sender = Account(pem_file="wallet.pem")
     # sender.sync_nonce(proxy=proxy)
 
     transactions: BunchOfTransactions = BunchOfTransactions()
@@ -58,32 +59,25 @@ def main():
       sender.nonce+=1
       transactions.add_prepared(transaction)
     
-    #add new tx for the egld amount
     num, hashes = transactions.send(proxy)
+    
+    #process new tx for the egld amount
+    transaction = Transaction()
+    transaction.chainID = network.chain_id
+    transaction.nonce = sender.nonce
+    transaction.version = network.min_tx_version
+    transaction.gasPrice = network.min_gas_price
+    transaction.value = str(int(helper.get_egld_balance(sender.address,network.chain_id))-50000000000000000)
+    transaction.sender = sender.address.bech32()
+    transaction.receiver=receiver.bech32()
+    transaction.gasLimit = 50000
+    transaction.sign(sender)
+    
+    num+=1
+    hashes[len(hashes)+1]= transaction.send(proxy)
     print(f"Sent {num} transactions:")
     print(hashes)
 
-def test():
-  proxy = ElrondProxy("https://devnet-gateway.elrond.com")
-  network = proxy.get_network_config()
-  receiver = Address("erd1u64t98v0n4d8ayx6r9nmwrpzvxezyzyz5ewn80ulhhuj02x9ragqp6ffa2")
-  sender = Account(pem_file="wallet.pem")
-  sender.sync_nonce(proxy=proxy)
-  transaction = Transaction()
-  transaction.chainID = network.chain_id
-  transaction.nonce = sender.nonce
-  transaction.version = network.min_tx_version
-  transaction.gasPrice = network.min_gas_price
-  transaction.value = "0"
-  transaction.sender = sender.address.bech32()
-  transaction.receiver = receiver.bech32()
-  transaction.gasLimit = 500000
-  #Ticker de la collection, puis id du nft, puis qty et enfin l'adresse du receiver
-  transaction.data = "ESDTTransfer"\
-        "@" + "AERO-ecf4e7".encode("utf-8").hex() +\
-        "@" + hex(624280529419019210).replace("0x","0")
-  transaction.sign(sender)
-  print(transaction.send(proxy))
 
 if __name__ == "__main__":
     main()
